d0134dba7fe8a3eb44634314601395c46c3427cf
Deepak Raj Gnanamuralidharan(UST <291493@ust.com>
2026-07-20 20:17:44 +0530

    screen shot fix

--- utils/AttachScreenshot.ts
+++ utils/AttachScreenshot.ts
@@ -2,5 +2,5 @@ import { allure } from 'allure-playwright';
 import { Page } from '@playwright/test';
 
 export async function attachScreenshot(page:Page, name: string){
-    await allure.attachment(name, await page.screenshot(), "imgage/png");
+    await allure.attachment(name, await page.screenshot(), "image/png");
 }
